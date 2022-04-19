e1e38784d2f4e02e71d90bc066860bdef8318d21
ltadaki <103495607+ltadaki@users.noreply.github.com>
2022-04-19 11:12:15 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . # # . .
+            # . . # .
+            # # # # .
+            # . . # .
+            # . . # .
+            `)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # # # . .
+            # . . # .
+            # # # . .
+            `)
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.clearScreen()
+    }
+})
